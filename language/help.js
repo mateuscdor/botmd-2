aecbfe2355cf822a51c9fa81f8a934fa4324872e
CarlosOp <87618898+carlospoponOp@users.noreply.github.com>
2022-04-09 11:17:17 -0500

    Update help.js

--- language/help.js
+++ language/help.js
@@ -265,3 +265,263 @@ exports.thanksto = () => {
 └───────
 `
 }
+
+//menu grupo
+exports.group = (prefix) => {
+return`
+┌── *Grupo Menu*
+│» ${prefix}linkgrupo
+│» ${prefix}ephemeral [opcion]
+│» ${prefix}setppgc [imagen]
+│» ${prefix}setname [text]
+│» ${prefix}setdesc [text]
+│» ${prefix}grupo [opcion]
+│» ${prefix}editinfo [opcion]
+│» ${prefix}add @user
+│» ${prefix}bug [reportar error]
+│» ${prefix}kick @user
+│» ${prefix}hidetag [text]
+│» ${prefix}tagall [text]
+│» ${prefix}antilink [on/off]
+│» ${prefix}mute [on/off]
+│» ${prefix}promote @user
+│» ${prefix}demote @user
+│» ${prefix}encuesta [text]
+│» ${prefix}votopositivo
+│» ${prefix}votonegativo
+│» ${prefix}checkvoto
+│» ${prefix}deletevoto
+└───────
+`
+}
+
+//downloads
+exports.downloads = (prefix) => {
+return`
+┌── *Downloader Menu*
+│» ${prefix}tiktoknowm [url]
+│» ${prefix}tiktokwm [url]
+│» ${prefix}tiktokmp3 [url]
+│» ${prefix}ytmp3 [url]
+│» ${prefix}mp3 [url
+│» ${prefix}mp4 [url]
+│» ${prefix}ytmp4 [url]
+│» ${prefix}getmusic [consulta]
+│» ${prefix}getvideo [consulta]
+└───────
+`
+}
+
+//Menu busquedas
+exports.search = (prefix) => {
+return`
+┌── *Search Menu*
+│» ${prefix}play [consulta]
+│» ${prefix}play2 [consulta]
+│» ${prefix}yts [consulta]
+│» ${prefix}google [consulta]
+│» ${prefix}gimage [consulta]
+│» ${prefix}pinterest [consulta]
+│» ${prefix}wallpaper [consulta]
+│» ${prefix}wikimedia [consulta]
+│» ${prefix}ytsearch [consulta]
+│» ${prefix}ringtone [consulta]
+└───────
+`
+}
+
+//Random anime
+exports.random = (prefix) => {
+return`
+┌── *Random Menu*
+│» ${prefix}coffe
+│» ${prefix}imgpareja
+│» ${prefix}anime
+│» ${prefix}waifu
+│» ${prefix}nekoo
+│» ${prefix}smug
+│» ${prefix}baka
+│» ${prefix}husbu
+│» ${prefix}cat
+│» ${prefix}foxgirl
+│» ${prefix}neko
+│» ${prefix}shinobu
+│» ${prefix}waifus (nsfw)
+│» ${prefix}nekos (nsfw)
+│» ${prefix}trap (nsfw)
+│» ${prefix}blowjob (nsfw)
+└───────
+`
+}
+
+//texpro 
+exports.logomaker = (prefix) => {
+return`
+Este menu suele contener errores de api, algunos logos no funcionarán!
+┌── *Text Pro Menu*
+│» ${prefix}3dchristmas
+│» ${prefix}3ddeepsea
+│» ${prefix}americanflag
+│» ${prefix}3dscifi
+│» ${prefix}3drainbow
+│» ${prefix}3dwaterpipe
+│» ${prefix}halloweenskeleton
+│» ${prefix}sketch
+│» ${prefix}bluecircuit
+│» ${prefix}space
+│» ${prefix}metallic
+│» ${prefix}fiction
+│» ${prefix}greenhorror
+│» ${prefix}transformer
+│» ${prefix}berry
+│» ${prefix}thunder
+│» ${prefix}magma
+│» ${prefix}3dcrackedstone
+│» ${prefix}3dneonlight
+│» ${prefix}impressiveglitch
+│» ${prefix}naturalleaves
+│» ${prefix}fireworksparkle
+│» ${prefix}matrix
+│» ${prefix}dropwater
+│» ${prefix}harrypotter
+│» ${prefix}foggywindow
+│» ${prefix}neondevils
+│» ${prefix}christmasholiday
+│» ${prefix}3dgradient
+│» ${prefix}blackpink
+│» ${prefix}gluetext
+└───────
+
+┌── *Photo Oxy Menu*
+│» ${prefix}shadow
+│» ${prefix}romantic
+│» ${prefix}smoke
+│» ${prefix}burnpapper
+│» ${prefix}naruto
+│» ${prefix}lovemsg
+│» ${prefix}grassmsg
+│» ${prefix}lovetext
+│» ${prefix}coffecup
+│» ${prefix}butterfly
+│» ${prefix}harrypotter
+│» ${prefix}retrolol
+└───────
+
+┌── *Ephoto Menu*
+│» ${prefix}ffcover
+│» ${prefix}crossfire
+│» ${prefix}galaxy
+│» ${prefix}glass
+│» ${prefix}neon
+│» ${prefix}beach
+│» ${prefix}blackpink
+│» ${prefix}igcertificate
+│» ${prefix}ytcertificate
+└───────
+`
+}
+
+//diversión
+exports.funny = (prefix) => {
+return`
+┌── *Fun Menu*
+│» ${prefix}amigos
+│» ${prefix}pareja
+│» ${prefix}math [modo]
+│» ${prefix}suitpvp [@tag]
+└──────
+`
+}
+
+//convertidor
+
+exports.convert = (prefix) => {
+return`
+┌── *Convertidor Menu*
+│» ${prefix}toimage
+│» ${prefix}removebg
+│» ${prefix}sticker
+│» ${prefix}emojimix
+│» ${prefix}tovideo
+│» ${prefix}togif
+│» ${prefix}tourl
+│» ${prefix}tovn
+│» ${prefix}tomp3
+│» ${prefix}toaudio
+│» ${prefix}ebinary
+│» ${prefix}dbinary
+│» ${prefix}styletext
+└───────
+`
+}
+
+//main
+exports.main = (prefix) => {
+return`
+┌── *Main Menu*
+│» ${prefix}ping
+│» ${prefix}owner
+│» ${prefix}Menu
+│» ${prefix}delete
+│» ${prefix}infochat
+│» ${prefix}quoted
+│» ${prefix}listpc
+│» ${prefix}listgc
+│» ${prefix}listonline
+└───────
+`
+}
+
+//database
+exports.database = (prefix) => {
+return`
+┌── *Database Menu*
+│» ${prefix}setcmd
+│» ${prefix}listcmd
+│» ${prefix}delcmd
+│» ${prefix}lockcmd
+│» ${prefix}addmsg
+│» ${prefix}listmsg
+│» ${prefix}getmsg
+│» ${prefix}delmsg
+└───────
+`
+}
+
+//editor de voz
+exports.voice = (prefix) => {
+return`
+┌── *Voice Changer*
+│» ${prefix}bass
+│» ${prefix}blown
+│» ${prefix}deep
+│» ${prefix}saturado
+│» ${prefix}fast
+│» ${prefix}fat
+│» ${prefix}nightcore
+│» ${prefix}reversa
+│» ${prefix}lento
+│» ${prefix}robot
+│» ${prefix}suave
+│» ${prefix}ardilla
+└───────
+`
+}
+
+//owner menu 
+exports.ownermenu = (prefix) => {
+return` 
+
+┌── *Owner Menu*
+│» ${prefix}chat [opcion]
+│» ${prefix}join [link]
+│» ${prefix}leave
+│» ${prefix}block @user
+│» ${prefix}unblock @user
+│» ${prefix}bcgroup [text]
+│» ${prefix}bcall [text]
+│» ${prefix}setppbot [imagen]
+│» ${prefix}setexif
+└───────
+`
+}
