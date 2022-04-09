14e9ebc40e07a7bae574088ff4da17407e1f3079
CarlosOp <87618898+carlospoponOp@users.noreply.github.com>
2022-04-08 20:39:13 -0500

    Update keyol.js

--- keyol.js
+++ keyol.js
@@ -1460,15 +1460,15 @@ break
                 if (/bass/.test(command)) set = '-af equalizer=f=54:width_type=o:width=2:g=20'
                 if (/blown/.test(command)) set = '-af acrusher=.1:1:64:0:log'
                 if (/deep/.test(command)) set = '-af atempo=4/4,asetrate=44500*2/3'
-                if (/earrape/.test(command)) set = '-af volume=12'
+                if (/saturado/.test(command)) set = '-af volume=12'
                 if (/fast/.test(command)) set = '-filter:a "atempo=1.63,asetrate=44100"'
                 if (/fat/.test(command)) set = '-filter:a "atempo=1.6,asetrate=22100"'
                 if (/nightcore/.test(command)) set = '-filter:a atempo=1.06,asetrate=44100*1.25'
-                if (/reverse/.test(command)) set = '-filter_complex "areverse"'
+                if (/reversa/.test(command)) set = '-filter_complex "areverse"'
                 if (/robot/.test(command)) set = '-filter_complex "afftfilt=real=\'hypot(re,im)*sin(0)\':imag=\'hypot(re,im)*cos(0)\':win_size=512:overlap=0.75"'
-                if (/slow/.test(command)) set = '-filter:a "atempo=0.7,asetrate=44100"'
-                if (/smooth/.test(command)) set = '-filter:v "minterpolate=\'mi_mode=mci:mc_mode=aobmc:vsbmc=1:fps=120\'"'
-                if (/tupai/.test(command)) set = '-filter:a "atempo=0.5,asetrate=65100"'
+                if (/lento/.test(command)) set = '-filter:a "atempo=0.7,asetrate=44100"'
+                if (/suave/.test(command)) set = '-filter:v "minterpolate=\'mi_mode=mci:mc_mode=aobmc:vsbmc=1:fps=120\'"'
+                if (/ardilla/.test(command)) set = '-filter:a "atempo=0.5,asetrate=65100"'
                 if (/audio/.test(mime)) {
                 m.reply(mess.wait)
                 let media = await yol.downloadAndSaveMediaMessage(quoted)
